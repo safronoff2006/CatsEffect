f705b3783692edc15e9f463b0ab02791f0074ee6
Валерий Сафронов <safronoff2006@debian.safronoff2006>
2024-12-15 15:40:26 +0300

    Commit 18 Signed-off-by: Валерий Сафронов <safronoff2006@debian.safronoff2006>

---
+++ CatsEffect_EssentialEffects_2/src/main/scala/Main.scala
@@ -0,0 +1,31 @@
+
+
+import cats.{Functor, Monad}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
+import cats.implicits._
+
+import java.util.concurrent.TimeUnit
+import scala.util.Try
+
+
+object Main1 extends App {
+  println("Изучаем Cats Effect - книга Essential Effects")
+
+  // https://github.com/inner-product/essential-effects-code
+
+  // В этой книге мы покажем изменения в коде в виде “различий”, которые вы можете увидеть в обзоре кода.
+  // Исходный код отображается красным цветом с префиксом -,
+  // а обновленная версия - зеленым цветом с префиксом +.
+
+  object part1 {
+
+  }
+
+
+
+}
+
