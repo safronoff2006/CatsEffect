01e4f3d6530b80298d0cc4484ad0c7831e0f7223
Валерий Сафронов <safronoff2006@debian.safronoff2006>
2024-12-25 15:53:36 +0300

    Commit 23 Signed-off-by: Валерий Сафронов <safronoff2006@debian.safronoff2006>

---
+++ CatsEffect_EssentialEffects_4_Chapter_4..Concurrent-control/src/main/scala/utils/debug.scala
@@ -0,0 +1,17 @@
+package utils
+
+import cats.effect._
+
+object debug {
+  /** Extension methods for an effect of type `IO[A]`. */
+  implicit class DebugHelper[A](ioa: IO[A]) {
+    /** Print to the console the value of the effect
+     * along with the thread it was computed on. */
+    def debugio: IO[A] =
+      for {
+        a <- ioa
+        tn = Thread.currentThread.getName
+        _ = println(s"[${Colorize.reversed(tn)}] $a")
+      } yield a
+  }
+}
